b661ce306a6b6e0b7001397160d6b11480644b2c
Bruno Tavares <bruno.exz@gmail.com>
2014-10-13 17:58:14 -0300

    Add support for Ubuntu notifications using dbus

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var OSXReporter = function(helper, logger, config) {
   var center = spawn(path.join(root, "/node_modules/node-osx-notifier/lib/node-osx-notifier.js"), [config_osx.port, config_osx.host]);
   log.info("OSX Notification Center reporter started at http://%s:%s", config_osx.host, config_osx.port);
   center.on('exit', function(code) {
-      log.info('node-osx-notifier exited with code ' + code);
+    log.info('node-osx-notifier exited with code ' + code);
   });
 
   this.adapters = [];
@@ -83,19 +83,75 @@ var OSXReporter = function(helper, logger, config) {
       log.error('error: ' + err.message);
     });
     req.end();
-  }  
+  }
 };
-
-if (process.platform !== 'darwin') {
-  // overwrite reporter with void function for non-OSX
-  OSXReporter = function(helper, logger) {}
-}
 
 OSXReporter.$inject = ['helper', 'logger', 'config'];
 
+var UbuntuReporter = function(helper, logger) {
+  var log = logger.create('reporter.ubuntu');
+
+  var notifications = null;
+  var notificationId = 0;
+
+  var sessionBus = require('dbus-native').sessionBus();
+  sessionBus.getService('org.freedesktop.Notifications').getInterface(
+    '/org/freedesktop/Notifications',
+    'org.freedesktop.Notifications',
+    function(err, service) {
+      notifications = service;
+    }
+  );
+
+  this.onBrowserComplete = function(browser) {
+    var results = browser.lastResult;
+    var time = helper.formatTimeInterval(results.totalTime);
+
+    var icon = null,
+    title = null,
+    message = null;
+
+    log.debug(results);
+
+    if (results.failed) {
+      icon = 'dialog-error';
+      title = util.format('FAILED - %s', browser.name);
+      message = util.format('%d/%d tests failed in %s.', results.failed, results.total, time);
+    }
+    else if (results.disconnected || results.error) {
+      icon = 'face-crying';
+      title = util.format('ERROR - %s', browser.name);
+      message = 'Test error';
+    }
+    else {
+      icon = 'emblem-default'; // Currently, this is a green tick mark. Didn't find better stock id.
+      title = util.format('PASSED - %s', browser.name);
+      message = util.format('%d tests passed in %s.', results.success, time);
+    }
+
+    if (notifications) {
+      notifications.Notify('', notificationId, icon, title, message, [], [], 5, function(err, id) {
+        notificationId = id;
+      });
+    } else {
+      log.info("Notification service not ready yet");
+    }
+  };
+};
+
+UbuntuReporter.$inject = ['helper', 'logger'];
+
+var notificationClass;
+
+if (process.platform === 'darwin') {
+  notificationClass = OSXReporter;
+} else {
+  notificationClass = UbuntuReporter;
+}
+
 // PUBLISH DI MODULE
 module.exports = {
-  'reporter:osx': ['type', OSXReporter]
+  'reporter:notification': ['type', notificationClass]
 };
 
 
